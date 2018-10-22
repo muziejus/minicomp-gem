f2ee21f8188ba3c7c44c4bab1f0cce93044f5543
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-10-22 14:19:31 -0400

    Initial lib settings.

---
+++ lib/minicomp.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# This is an anti-pattern
+# $LOAD_PATH.unshift __dir__ # For use/testing when no gem is installed
+
+# Require all of the Ruby files in the given directory.
+#
+# path - The String relative path from here to the directory.
+#
+# Returns nothing.
+def require_all(path)
+  glob = File.join(__dir__, path, "*.rb")
+  Dir[glob].sort.each do |f|
+    require f
+  end
+end
+
+# stlib
+
+# third-party
+# require "safe_yaml/load"
+
+# SafeYAML::OPTIONS[:suppress_warnings] = true
+
+module Minicomp
+  autoload :VERSION, "minicomp/version"
+end
---
+++ lib/minicomp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Minicomp
+  VERSION = "0.0.0"
+end
